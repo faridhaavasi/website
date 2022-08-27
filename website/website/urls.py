0f2af1bf50ecd93f1d96b50a51d8af89ea2e3440
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-08-27 14:58:23 +0430

    added urls paa account

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog_app.urls'))
+    path('',include('blog_app.urls')),
+    path('account',include('account.urls')),
+    
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
