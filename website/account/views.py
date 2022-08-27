480f0c73ba406d903ef63cb1d7c684f0e2c86f46
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-08-27 23:32:09 +0430

    login logout ok!

--- website/account/views.py
+++ website/account/views.py
@@ -10,6 +10,7 @@ def login_user(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+       
         if user is not None:
             login(request, user)
             return redirect('/')     
