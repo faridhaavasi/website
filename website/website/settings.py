67b9087096085597ac79765fb95a245dffae7f91
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-08-27 15:14:17 +0430

    set static files

--- website/website/settings.py
+++ website/website/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staic')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'assets'),
+    os.path.join(BASE_DIR,'static'),
 ]
 MEDIA_URL='media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
