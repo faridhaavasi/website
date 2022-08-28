0827be95e8274b211b20b6c36f7cd91fa7dc13b3
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-08-29 03:01:19 +0430

    register view ok

--- website/account/views.py
+++ website/account/views.py
@@ -1,5 +1,10 @@
+
+from distutils.log import error
+from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
+
+
 
 # Create your views here.
 def login_user(request):
@@ -24,7 +29,23 @@ def logout_user(request):
 def register_user(request):
     if request.user.is_authenticated:
         return redirect('/')
+    context={
+        'errors':[]
+    }    
 
-    return render(request,'account/register.html',{})    
+    if request.method ==  'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        user=User.objects.create(username=username,password=password,email=email)
+        if password != password2 :
+            complex.get['errors'].append('پسورد و تکرار آن یکی نیست')
+            return render(request,'account/register.html',context)
+        else:
+            login(request,user)
+            return redirect('/')    
+        
+    return render(request,'account/register.html',context)    
 
 
