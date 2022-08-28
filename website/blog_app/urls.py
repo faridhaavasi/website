09c459e06480070e290218cc856885dea3b6117b
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-08-29 02:26:49 +0430

    fixed bog

--- website/blog_app/urls.py
+++ website/blog_app/urls.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+
 from django.urls import path
 from . import views
 
--- website/website/urls.py
+++ website/website/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog_app.urls')),
-    path('account',include('account.urls')),
+    path('account/',include('account.urls')),
     
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
